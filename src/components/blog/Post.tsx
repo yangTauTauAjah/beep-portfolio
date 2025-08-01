d5d9c6deab055fe5a9ae9cf88607df1b6729a486
Habib Anwash <tesakun6464@gmail.com>
2025-08-01 21:48:52 +0700

    fix error on build

--- src/components/blog/Post.tsx
+++ src/components/blog/Post.tsx
@@ -24,7 +24,7 @@ export default function Post({ post, thumbnail, direction }: PostProps) {
                 direction={direction}
                 radius="l"
                 className={styles.hover}
-                mobileDirection="column"
+                /* mobileDirection="column" */
                 fillWidth>
                 {post.metadata.image && thumbnail && (
                     <Media
